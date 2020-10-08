6c49b22f27aad6b3c35ddb7ea5d1cb1d858980ce
Thomas Druart <thomas.druart.59@gmail.com>
2020-10-08 09:39:17 +0200

    menu corrigé

--- postRecipe.js
+++ postRecipe.js
@@ -1,30 +1,4 @@
-// form validation
-
-let validate = {
-    if (document.recipeForm.userName.value == '') {
-        alert("Tu as oublié de renseigner ton nom");
-        document.recipeForm.userName.focus();
-        return false;
-    }
-    if (document.recipeForm.experience.value == '') {
-        alert("Parle nous de ton expérience");
-        document.recipeForm.experience.focus();
-        return false;
-    }
-    if (document.recipeForm.userRecipe.value == '') {
-        alert ("N'oublie pas d'indiquer le nom de ta recette");
-        document.recipeForm.userRecipe.focus();
-        return false;
-    }
-    if (document.recipeForm.ingredients.value == '') {
-        alert ("Tu as oublié de renseigner les ingrédients");
-        document.recipeForm.ingredients.focus();
-        return false;
-    }
-    if (document.recipeForm.detailRecipe.value == '') {
-        alert ("N'oublie pas de détailler les étapes de ta recette");
-        document.recipeForm.detailRecipe.focus();
-        return false;
-    }
-    return true;
-};
+document.addEventListener("scroll", function() {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
